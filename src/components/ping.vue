ca710d3288dacc2b4800f2f1efeb13ea1b75fe32
wangxiao9 <1012883981@qq.com>
2019-11-26 17:46:45 +0800

    登录样式添加

---
+++ src/components/ping.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'ping',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    getMessage () {
+      const path = 'http://127.0.0.1:5000/cms/ping/'
+      axios
+        .get(path)
+        .then(res => {
+          this.msg = res.data
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
+  },
+  created () {
+    this.getMessage()
+  }
+}
+</script>
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import ping from './components/ping.vue'
+import Login from './components/Login.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // { path: '/', name: 'ping', component: ping },
+    { path: '/', redirect: '/login' },
+    { path: '/login', name: 'Login', component: Login }
+  ]
+})
