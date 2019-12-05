809db370f8328e0d529caaea87b5a2c899bfba6e
root <wangxiao@jiangduoduo.com>
2019-12-05 17:48:36 +0800

    layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,61 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+  <el-container>
+    <!--头部区域-->
+    <el-header>
+      <div>
+        <span>测试开发管理平台</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!--页面主体-->
+    <el-container>
+      <!--左边侧边栏-->
+      <el-aside width="200px">
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :router="true">
+         <!--一级菜单-->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>用户管理</span>
+            </template>
+            <!--二级菜单-->
+            <el-menu-item-group>
+              <el-menu-item index="/users">
+                <template slot="title">
+                  <i class="el-icon-menu"></i>
+                  <span>用户列表</span>
+                </template>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>导航一</span>
+            </template>
+            <!--二级菜单-->
+            <el-menu-item-group>
+              <el-menu-item index="2-1">
+                <template slot="title">
+                  <i class="el-icon-menu"></i>
+                  <span>导航二</span>
+                </template>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!--右侧内容-->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   methods: {
     // 退出登录
-    logout () {
+    logout() {
       // 1.清除token
       window.sessionStorage.clear()
       // 2.跳转到登录页面
@@ -19,4 +66,24 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Header, Aside, Main, Container, Menu, Submenu, MenuItem, MenuItemGroup } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Container)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+
 // 挂载弹框属性到全局变量中
 Vue.prototype.$message = Message
